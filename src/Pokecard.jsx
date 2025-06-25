07e5ea82c7353b215ceb023084ed2c4c7b737026
Hadjati Darius <72970048+HDarius90@users.noreply.github.com>
2025-06-25 17:18:21 +0200

    creating pokecard component

---
+++ src/Pokecard.jsx
@@ -0,0 +1,18 @@
+import './Pokecard.css';
+
+const POKE_API =
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+const Pokecard = ({ id, name, type, exp }) => {
+  const imgSrc = `${POKE_API}${id}.png`;
+  return (
+    <div className="Pokecard">
+      <h1>{name}</h1>
+      <img src={imgSrc} alt={name} />
+      <div>Type: {type}</div>
+      <div>EXP: {exp}</div>
+    </div>
+  );
+};
+
+export default Pokecard;
